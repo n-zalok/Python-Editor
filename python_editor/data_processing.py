dd35a7337237bc6b4c06fba152216b62a80b003f
n-zalok <nooreldeen2002@gmail.com>
2026-02-09 22:13:09 +0200

    splitting data

--- python_editor/data_processing.py
+++ python_editor/data_processing.py
@@ -2,6 +2,7 @@ import pandas as pd
 import subprocess
 import tempfile
 import json
+from sklearn.model_selection import GroupShuffleSplit
 
 
 def get_pylint_text(row: pd.Series) -> str:
@@ -31,3 +32,13 @@ def get_pylint_score(row: pd.Series) -> float:
         return -1
     else:
         return pylint_json["statistics"]["score"]
+
+
+def split_by_developer(df: pd.DataFrame, test_size: float, random_state: int) -> pd.DataFrame:
+    df["developer"] = df.apply(lambda row: row["repo_name"].split("/")[0], axis=1)
+
+    splitter = GroupShuffleSplit(n_splits=1, test_size=test_size, random_state=random_state)
+    splits = splitter.split(df, groups=df["developer"])
+    train_idx, test_idx = next(splits)
+
+    return df.iloc[train_idx, :], df.iloc[test_idx, :]
