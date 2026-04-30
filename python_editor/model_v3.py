74ccf45c95473727660224049514ad7619b2b67b
n-zalok <nooreldeen2002@gmail.com>
2026-04-30 17:20:11 +0300

    third model retraining

--- python_editor/model_v3.py
+++ python_editor/model_v3.py
@@ -1,8 +1,8 @@
 import pickle
 import pandas as pd
 import numpy as np
-from python_editor.data_processing import has_executable_code, vectorize_code
-from python_editor.feature_generation_v2 import generate_features
+from python_editor.data_processing import has_executable_code
+from python_editor.feature_generation_v2 import generate_transformed_features
 
 
 with open("../models/model_v3.pkl", "rb") as f:
@@ -11,11 +11,11 @@ with open("../models/model_v3.pkl", "rb") as f:
 
 def get_model_prediction_from_text(row: pd.Series)-> float:
     if has_executable_code(row):
-        features = generate_features(row)
+        features = generate_transformed_features(row)
         vectorized_features = np.array(list(features.values())).reshape(1, -1)
 
         prediction = MODEL.predict(vectorized_features)
 
-        return prediction[0].item()
+        return features, prediction[0].item()
     else:
-        return -1
+        return {}, -1
